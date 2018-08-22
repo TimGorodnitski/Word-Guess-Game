0cb514676c056b46d084559d54b75aa55aae94b0
TimG <timgm3@gmail.com>
2018-08-21 19:08:00 -0700

    Javascript added current word display function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,29 +1,24 @@
-
-console.log("test top");
-
-
-
 let answers = ["bill murray", "someone else"];
 let solution = document.getElementById("solutionpic");
 let chosenWord = answers[Math.floor(Math.random() * answers.length)];
+let display = "";
+let guesses = "";
+
 
 function solve() {
     document.getElementById("solution").innerHTML = `<img src="https://www.fillmurray.com/300/300" id="solutionpic">`
 }
-
-function intoUnderscores(chosenword) {
-    var result = chosenWord.split("");
-};
-
 
 
 document.onkeyup = function(event) {
     let guess = event.key;
 
 
+    for (i = 0, x = chosenWord.length; i < x; i += 1) {
+        if (guesses.indexOf(chosenWord[i]) >= 0) { display += chosenWord[i]} else {
+            display += "_";
+        }
+    }
+
 
 };
-
-
-console.log("test bottom");
-console.log(chosenWord);
