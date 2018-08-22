d2a838dbf53e1b751aa7f3ee7fa0ea71343c76ca
TimG <timgm3@gmail.com>
2018-08-21 18:15:17 -0700

    Laying out javascript

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,2 +1,29 @@
-let answers = [];
 
+console.log("test top");
+
+
+
+let answers = ["bill murray", "someone else"];
+let solution = document.getElementById("solutionpic");
+let chosenWord = answers[Math.floor(Math.random() * answers.length)];
+
+function solve() {
+    document.getElementById("solution").innerHTML = `<img src="https://www.fillmurray.com/300/300" id="solutionpic">`
+}
+
+function intoUnderscores(chosenword) {
+    var result = chosenWord.split("");
+};
+
+
+
+document.onkeyup = function(event) {
+    let guess = event.key;
+
+
+
+};
+
+
+console.log("test bottom");
+console.log(chosenWord);
