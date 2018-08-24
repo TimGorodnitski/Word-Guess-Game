aa4991e8f347dacb5acba1698ab5179f059cae19
TimG <timgm3@gmail.com>
2018-08-23 17:24:40 -0700

    Lives working, need to make a reset

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,14 @@
+
+
+// array of possible answers
 let answers = ["bill murray"];
+
 let solution = document.getElementById("solutionpic");
 let chosenWord = answers[Math.floor(Math.random() * answers.length)];
 var display = "";
 var guesses = " ";
 var wins = 0;
-var lives = 10;
+var lives = 11;
 
 
 function solve() {
@@ -13,22 +17,30 @@ function solve() {
 }
 
 
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
+    oldDisplay = display;
     display = "";
     guess = event.key;
     guesses += guess;
 
     for (i = 0; i < chosenWord.length; i++) {
 
-        if (guesses.indexOf(chosenWord[i]) !== -1 ) {display += chosenWord[i];} else {display += "_"; lives--;};
-        
-
+        if (guesses.indexOf(chosenWord[i]) !== -1) { display += chosenWord[i]; } else { display += "_"; };
     };
 
-    if (display === chosenWord) {solve();}
+
+    if(oldDisplay===display) {lives--;};
+
+    oldDisplay="";
+
+    if(lives===0){display="GAME OVER!"};
+
+    if (display === chosenWord) {
+        solve();
+    }
 
     document.getElementById("currentWordField").innerText = display;
     document.getElementById("usedLettersField").innerText = guesses;
-
-
+    document.getElementById("livesField").innerText = lives;
+    document.getElementById("winsField").innerText = wins;
 };
